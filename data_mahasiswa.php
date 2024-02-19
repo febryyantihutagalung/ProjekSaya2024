45dc2214e195732a3422e4e06757927bf899e3b3
febryyantihutagalung <159239484+febryyantihutagalung@users.noreply.github.com>
2024-02-19 15:26:51 +0700

    Update data_mahasiswa.php
    Update data semester

--- data_mahasiswa.php
+++ data_mahasiswa.php
@@ -1,6 +1,7 @@
 <?php
 echo "<h1>Data Mahasiswa</h1>";
 echo "NIM : 22110041 <br>";
-echo "Nama Lengkap : Febry Yanti Hutagalung";
-echo "Kelas : 22 MI A";
+echo "Nama Lengkap : Febry Yanti Hutagalung<br>";
+echo "Kelas : 22 MI A<br>";
+echo "Semester Genap";
 ?>
